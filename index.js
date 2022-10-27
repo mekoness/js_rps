11d8b35714dd21cd315d4d756c19c4deecc2c268
mekoness <mekoness@gmail.com>
2022-10-26 17:47:21 -0700

    refactoring part 2

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-const comChoiceEl = document.getElementById("com-choice");
-const userChoiceEl = document.getElementById("user-choice");
-const gameTextEl = document.getElementById("game-text");
-const playRock = document.getElementById("user-rock")
-const playPaper = document.getElementById("user-paper")
-const playScissors = document.getElementById("user-scissors")
-const endBtn = document.getElementById("end-btn")
-// const options = ["💎", "📃", "✂️"];
 const options = ["rock", "paper", "scissors"];
+
 let isPlaying = false;
 let wins = 0;
 let losses = 0;
@@ -14,52 +7,57 @@ let draws = 0;
 let userChoice = "";
 let comChoice = "";
 
-playRock.addEventListener("click", function(){
-    isPlaying = true;
-    userChoice = "💎";
-    userChoiceEl.innerHTML = `<img src="img/rock.png">`;
+const comChoiceEl = document.querySelector("#com-choice");
+const userChoiceEl = document.querySelector("#user-choice");
+const gameTextEl = document.querySelector("#game-text");
+const playRock = document.querySelector("#user-rock")
+const playPaper = document.querySelector("#user-paper")
+const playScissors = document.querySelector("#user-scissors")
+
+// User Choice
+
+function userPlay(option) {
+	isPlaying = true;
+	userChoice = option;
+	userChoiceEl.innerHTML = `<img src="img/${userChoice}.png" id="user-choice">`;
     if (isPlaying) {
         comPlay();
         theGame();
     };
+};
+
+playRock.addEventListener("click", () => {
+	userPlay("rock");
 });
 
-playPaper.addEventListener("click", function(){
-    isPlaying = true;
-    userChoice = "📃";
-    userChoiceEl.innerHTML = `<img src="img/paper.png">`;
-    if (isPlaying) {
-        comPlay();
-        theGame();
-    };
+playPaper.addEventListener("click", () => {
+	userPlay("paper");
 });
 
-playScissors.addEventListener("click", function(){
-    isPlaying = true;
-    userChoice = "✂️";
-    userChoiceEl.innerHTML = `<img src="img/scissors.png">`;
-    if (isPlaying) {
-        comPlay();
-        theGame();
-    };
+playScissors.addEventListener("click", () => {
+	userPlay("scissors");
 });
+
+// Com Choice
 
 function comPlay() {
     comChoice = options[ Math.floor( Math.random() * options.length ) ];
-    comChoiceEl.textContent = comChoice;
+    comChoiceEl.innerHTML = `<img src="img/${comChoice}.png" id="com-choice">`;
 };
+
+// Game Function
 
 function theGame() {
     if (comChoice === userChoice) {
         draws ++;
         gameTextEl.textContent = "Draw!";
-    } else if (comChoice === "💎" && userChoice === "📃") {
+    } else if (comChoice === "rock" && userChoice === "paper") {
         wins ++;
         gameTextEl.textContent = "You win!";
-    } else if (comChoice === "📃" && userChoice === "✂️") {
+    } else if (comChoice === "paper" && userChoice === "scissors") {
         wins ++;
         gameTextEl.textContent = "You win!";
-    } else if (comChoice === "✂️" && userChoice === "💎") {
+    } else if (comChoice === "scissors" && userChoice === "rock") {
         wins ++;
         gameTextEl.textContent = "You win!";
     } else {
@@ -67,6 +65,10 @@ function theGame() {
         gameTextEl.textContent = "You lose!";
     };
 };
+
+// End Game
+
+const endBtn = document.getElementById("end-btn")
 
 endBtn.addEventListener("click", function(){
     isPlaying = false;
